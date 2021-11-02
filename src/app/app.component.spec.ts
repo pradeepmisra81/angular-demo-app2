d1510297e48d80366902b20e5858bd0d12ddbabe
pradeep <pradeep.misra@gmail.com>
2021-11-02 20:59:36 +0530

    Updated title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-demo-app1'`, () => {
+  it(`should have as title 'angular-demo-app2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-demo-app1');
+    expect(app.title).toEqual('angular-demo-app2');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo-app1';
+  title = 'angular-demo-app2';
 }
